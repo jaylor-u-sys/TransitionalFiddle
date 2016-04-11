2aaab8e2ce9deadce720aa85e3f0581fc60a2e76
jaylor-u-sys <jaylor@unlimitedsystems.com>
2016-04-11 17:09:24 -0400

    'packagejson'

---
+++ app/expandedcard/expanded-card-tab.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'ec-tab',
+  styleUrls = ['app/expandedcard/expanded-card-tab.css'],
+  templateUrl = 'app/expandedcard/expanded-card-tab.html'
+})
+export class expandedCardTab {
+  
+}
