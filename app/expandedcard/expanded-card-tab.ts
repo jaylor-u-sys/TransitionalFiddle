0568fe439d82f664ae0705ef7a6ef1cfe26aa7d5
jaylor-u-sys <jaylor@unlimitedsystems.com>
2016-04-12 10:32:51 -0400

    Sytax Error fix

--- app/expandedcard/expanded-card-tab.ts
+++ app/expandedcard/expanded-card-tab.ts
@@ -2,9 +2,9 @@ import { Component } from 'angular2/core';
 
 @Component({
   selector: 'ec-tab',
-  styleUrls = ['app/expandedcard/expanded-card-tab.css'],
-  templateUrl = 'app/expandedcard/expanded-card-tab.html'
+  styleUrls: ['app/expandedcard/expanded-card-tab.css'],
+  templateUrl: 'app/expandedcard/expanded-card-tab.html'
 })
 export class expandedCardTab {
-  
+
 }
